9bc07f31527a8ce6e55a3a6e04810383d2e59ce9
Ben McKernan <ben-mckernan@users.noreply.github.com>
2019-08-23 08:41:41 +0200

    feat: Add references-empty-enum rule
    Add rule that ensures references are always or never empty for a given
    list of commit types.

---
+++ references-empty-enum.js
@@ -0,0 +1,18 @@
+const ensure = require("@commitlint/ensure");
+const message = require("@commitlint/message");
+
+module.exports = function (parsed, when = "never", value) {
+    const { references, type } = parsed;
+
+    if (!type || !ensure.enum(type, value)) {
+        return [true];
+    }
+
+    const negated = when === "always";
+    const empty = references.length === 0;
+
+    return [
+        negated ? empty : !empty,
+        message(["references", negated ? "must" : "must not", `be empty when type is one of [${value.join(", ")}]`])
+    ];
+};
